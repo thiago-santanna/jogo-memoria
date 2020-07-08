4ce6734cae12c354cf79bdba9402c54ba47165d7
xthiagosant <uzmaki_thiago@outlook.com>
2020-07-08 16:27:57 -0300

    funcao para geração de numeros para o jogo

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+function shuffle(array) {
+    for (let indexOld = array.length - 1; indexOld > 0; indexOld--) {
+        let indexNew = Math.floor(Math.random() * (indexOld + 1));
+        let temp = array[indexOld];
+        array[indexOld] = array[indexNew];        
+        array[indexNew] = temp;
+    }
+    return array;
+}
+let numBox = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
 const boxItens = document.querySelectorAll('.itemBox')
 
 const reiniciaBox = () =>{
-    boxItens.forEach( item => {
-        item.innerHTML = item.id
+    shuffle(numBox)
+    boxItens.forEach( item => {      
+        item.innerHTML = numBox[item.id]
     })
 }
 
 const mostrarBox = (props) => {
     alert(props)
 }
-
-function getRandon(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-console.log(getRandon(1,16))
