1c265a9519c571cc0d0de507d62890ef2d270f35
xthiagosant <uzmaki_thiago@outlook.com>
2020-07-08 19:58:16 -0300

    feito a mudança de cor ao selecionar

--- index.js
+++ index.js
@@ -7,16 +7,37 @@ function shuffle(array) {
     }
     return array;
 }
-let numBox = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
-const boxItens = document.querySelectorAll('.itemBox')
 
-const reiniciaBox = () =>{
+function carregarBox(){
     shuffle(numBox)
-    boxItens.forEach( item => {      
+    boxItens.forEach( item => {
         item.innerHTML = numBox[item.id]
+        item.style.backgroundColor = '#4CAF50';
     })
 }
 
+function mudaCor(idx){
+    boxItens.forEach( item => {
+        if(item.id === idx){
+            item.style.backgroundColor = '#fff'
+        }
+    })
+}
+
+let numBox = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
+let boxItens = document.querySelectorAll('.itemBox')
+let tentativas = 0
+
+const reiniciaBox = () =>{
+    carregarBox()
+}
+
 const mostrarBox = (props) => {
-    alert(props)
+    mudaCor(props)
+    tentativas++
+    if(tentativas === 2){
+        console.log('testar')
+        console.log('mudar_ou_não')
+        tentativas = 0
+    }
 }
