e5c159ac754086523f3096beeb6af3ba6b2ee17c
xthiagosant <uzmaki_thiago@outlook.com>
2020-07-28 05:46:59 -0300

    atualizacao

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function shuffle(array) {
     for (let indexOld = array.length - 1; indexOld > 0; indexOld--) {
-        let indexNew = Math.floor(Math.random() * (indexOld + 1));
-        let temp = array[indexOld];
-        array[indexOld] = array[indexNew];        
-        array[indexNew] = temp;
+        let indexNew = Math.floor(Math.random() * (indexOld + 1))
+        let temp = array[indexOld]
+        array[indexOld] = array[indexNew]
+        array[indexNew] = temp
     }
     return array;
 }
@@ -12,7 +12,7 @@ function carregarBox(){
     shuffle(numBox)
     boxItens.forEach( item => {
         item.innerHTML = numBox[item.id]
-        item.style.backgroundColor = '#4CAF50';
+        item.style.backgroundColor = '#4CAF50'
     })
 }
 
@@ -20,13 +20,44 @@ function mudaCor(idx){
     boxItens.forEach( item => {
         if(item.id === idx){
             item.style.backgroundColor = '#fff'
+            saveSelected(item.id, item.innerText)
         }
     })
 }
 
+function retornaCor(idx, idy){
+    boxItens.forEach( item => {
+        if(item.id === idx){
+            item.style.backgroundColor = '#4CAF50'
+        }   
+        if(item.id === idy){
+            item.style.backgroundColor = '#4CAF50'
+        }
+    })
+}
+
+function saveSelected(key, val){
+    numBoxSelect.push({
+        "Id": key,
+        "Valor": val
+    })
+}
+
+function verificaBox(num){    
+    let soma = numBoxSelect.reduce(( prevVal, box) => prevVal + Number(box.Valor) , 0)
+    let igual = Number((soma / 2)) === Number(num)
+    if(!igual){
+        retornaCor(numBoxSelect[0].Id, numBoxSelect[1].Id)
+    }
+    numBoxSelect.pop()
+    numBoxSelect.pop()
+}
+
 let numBox = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
+let numBoxSelect = []
 let boxItens = document.querySelectorAll('.itemBox')
 let tentativas = 0
+
 
 const reiniciaBox = () =>{
     carregarBox()
@@ -34,10 +65,7 @@ const reiniciaBox = () =>{
 
 const mostrarBox = (props) => {
     mudaCor(props)
-    tentativas++
-    if(tentativas === 2){
-        console.log('testar')
-        console.log('mudar_ou_não')
-        tentativas = 0
+    if(numBoxSelect.length === 2){    
+        setTimeout(() => verificaBox(numBoxSelect[0].Valor), 1000)
     }
 }
